4057297db5a7c57af173098a4431ce9cf2761178
Semeton <balogunsemeton@gmail.com>
2024-02-21 19:39:16 +0100

    feat(core): dashboard components

---
+++ src/views/home/components/QuickAccess.tsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { faVault } from "@fortawesome/free-solid-svg-icons";
+
+class QuickAccess extends Component {
+  render() {
+    const styles = "bg-gray-800 p-3 rounded-md border border-gray-600";
+    return (
+      <div className="mt-3 mb-5">
+        <div className="grid grid-cols-2 gap-3">
+          <div className={styles}>
+            <div className="mb-2">
+              <FontAwesomeIcon icon={faUserSecret} size="lg" />
+            </div>
+            <b>Secure Messaging</b>
+            <p className="text-sm p-0">
+              Zero-knowledge-proof encrypted messages
+            </p>
+          </div>
+          <div className={styles}>
+            <div className="mb-2">
+              <FontAwesomeIcon icon={faVault} size="lg" />
+            </div>
+            <b>Secure Vault</b>
+            <p className="text-sm p-0">
+              Encrypted sensitive data with secure secret
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default QuickAccess;
