3fff27446095f40f575b2f13d10a0266cb8a9eb3
Semeton <balogunsemeton@gmail.com>
2024-02-28 10:48:17 +0100

    mod: sidebar with profile pics, name and email

--- src/views/layout/SideBar.tsx
+++ src/views/layout/SideBar.tsx
@@ -1,45 +1,92 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faUserGear,
-  faUserSecret,
-  faVault
+  faUser,
+  faGear,
+  faRightFromBracket,
+  faEyeSlash
 } from "@fortawesome/free-solid-svg-icons";
+import User from "../../services/api/auth/User";
+import { userDetails } from "../../lib/UserDetails";
 
-const SideBar = () => (
-  <aside className="sidebar fixed top-0 left-0 z-9999999 w-64 h-full bg-gray-900 text-white">
-    <nav className="mt-10">
-      <Link
-        to="/profile"
-        className="flex items-center py-2 px-8 hover:bg-gray-700"
-      >
-        <FontAwesomeIcon icon={faUserSecret} size="lg" />
-        <span className="mx-4">Profile</span>
-      </Link>
-      <Link
-        to="/account-info"
-        className="flex items-center py-2 px-8 hover:bg-gray-700"
-      >
-        <FontAwesomeIcon icon={faUserGear} size="lg" />
-        <span className="mx-4">Settings</span>
-      </Link>
-      <Link
-        to="/chats"
-        className="flex items-center py-2 px-8 hover:bg-gray-700"
-      >
-        <FontAwesomeIcon icon={faUserSecret} size="lg" />
-        <span className="mx-4">Chats</span>
-      </Link>
-      <Link
-        to="/vault"
-        className="flex items-center py-2 px-8 hover:bg-gray-700"
-      >
-        <FontAwesomeIcon icon={faVault} size="lg" />
-        <span className="mx-4">Vault</span>
-      </Link>
-    </nav>
-  </aside>
-);
+const SideBar: React.FC = () => {
+  const user = userDetails;
+
+  const logout = (e: any): any => {
+    e.preventDefault();
+    User.logout();
+  };
+
+  return (
+    <aside className="sidebar fixed top-0 left-0 z-9999999 w-full h-full bg-gray-900 text-white">
+      <div className="px-8">
+        <div className="mx-auto items-center mt-6">
+          <div className="text-indigo-500 rounded-full h-16 w-16 bg-gray-950 text-center justify-center flex items-center mr-4">
+            <FontAwesomeIcon
+              className="text-center mx-auto"
+              icon={faUser}
+              size="2xl"
+            />
+          </div>
+          <div className="mt-1">
+            <h3 className="font-bold text-xl">{user.name}</h3>
+            <p className="text-sm">{user.email}</p>
+          </div>
+        </div>
+        <nav className="mt-4 text-xl text-center  mx-auto">
+          <Link
+            to="/dashboard"
+            className="flex items-center py-2 hover:bg-gray-700"
+          >
+            <div className="grid grid-cols-6 items-center gap-5">
+              <FontAwesomeIcon
+                className="text-center col-span-2 w-full"
+                icon={faUser}
+              />
+              <span className="col-span-4 mr-4">Profile</span>
+            </div>
+          </Link>
+          <Link
+            to="/dashboard"
+            className="flex items-center py-2 hover:bg-gray-700"
+          >
+            <div className="grid grid-cols-6 items-center gap-5">
+              <FontAwesomeIcon
+                icon={faGear}
+                className="text-center col-span-2 w-full"
+              />
+              <span className="col-span-4">Settings</span>
+            </div>
+          </Link>
+          <Link
+            to="/chats"
+            className="flex items-center py-2 hover:bg-gray-700"
+          >
+            <div className="grid grid-cols-6 items-center gap-5">
+              <FontAwesomeIcon
+                icon={faEyeSlash}
+                className="text-center col-span-2 w-full"
+              />
+              <span className="col-span-4">Privacy</span>
+            </div>
+          </Link>
+          <div
+            className="mt-10 flex items-center py-2 hover:bg-gray-700 my-auto"
+            onClick={logout}
+          >
+            <div className="grid grid-cols-6 items-center gap-5">
+              <FontAwesomeIcon
+                icon={faRightFromBracket}
+                className="text-center col-span-2 w-full pr-1"
+              />
+              <span className="col-span-4">Logout</span>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </aside>
+  );
+};
 
 export default SideBar;
