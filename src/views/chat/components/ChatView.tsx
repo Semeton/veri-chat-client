288ed875fc91c5d4bc3621d626370764dd79b194
Semeton <balogunsemeton@gmail.com>
2024-03-31 12:26:07 +0100

    feat: chat messages ui

--- src/views/chat/components/ChatView.tsx
+++ src/views/chat/components/ChatView.tsx
@@ -4,6 +4,12 @@ import { baseUrl } from "../../../services/api/urls/Links";
 import { Endpoints } from "../../../services/api/urls/Endpoints";
 import Alerts from "../../../util/alerts/Alerts";
 import { useEffect } from "react";
+import {
+  faArrowLeftLong,
+  faGear,
+  faPaperPlane,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ChatView: React.FC = () => {
   const { id, secret } = useParams();
@@ -38,25 +44,57 @@ const ChatView: React.FC = () => {
   return (
     <div className="fixed">
       <div className="flex h-screen flex-col justify-between bg-gray-900">
-        <header className="mb-2 grid grid-cols-3 items-center justify-between text-white">
+        <header className="mb-2 grid grid-cols-3 items-center justify-between bg-gray-950 text-white">
           <div
             onClick={() => window.history.back()}
             className="flex h-16 items-center p-6"
           >
-            Back
+            <FontAwesomeIcon icon={faArrowLeftLong} />
           </div>
           <div className="mx-auto">
-            <div className="flex items-center justify-center rounded-full bg-gray-950 text-center text-indigo-500">
+            <div className="flex items-center justify-center rounded-full text-center text-lg font-bold text-indigo-500">
               Full Name
             </div>
           </div>
-          <div className="flex h-16 items-center justify-end p-6">Settings</div>
+          <div className="flex h-16 items-center justify-end p-6">
+            <FontAwesomeIcon icon={faGear} size="lg" />
+          </div>
         </header>
-        <main id="main" className="mb-auto overflow-y-scroll p-3 px-5">
-          {id}
+        <main className="mb-auto mt-2 overflow-y-scroll">
+          <div className="float-left my-1 max-w-xs rounded-lg bg-indigo-500 p-2 text-white">
+            Message bubble on the left. Message bubble on the left
+          </div>
+          <div className="float-left my-1 max-w-xs rounded-lg bg-indigo-500 p-2 text-white">
+            Message bubble on the left. Message bubble on the left
+          </div>
+          <div className="float-right my-1 max-w-xs rounded-lg bg-gray-800 p-2 text-white">
+            Message bubble on the right. Message bubble on the right
+          </div>
+          <div className="float-left my-1 max-w-xs rounded-lg bg-indigo-500 p-2 text-white">
+            Message bubble on the left. Message bubble on the left
+          </div>
+          <div className="float-right my-1 max-w-xs rounded-lg bg-gray-800 p-2 text-white">
+            Message bubble on the right. Message bubble on the right
+          </div>
+          <div className="float-right my-1 max-w-xs rounded-lg bg-gray-800 p-2 text-white">
+            Message bubble on the right. Message bubble on the right
+          </div>
         </main>
-        <div className="bottom-0 h-20 w-screen bg-gray-950 pt-3">
-          <div className="flex items-center justify-around text-center text-sm text-white"></div>
+        <div className="bottom-0 h-24 w-screen bg-gray-950 pt-5">
+          <div className="flex items-center justify-around text-center text-sm text-white">
+            <input
+              type="text"
+              name="message"
+              id="message"
+              className="focus:ring-primary-600 focus:border-primary-600 ml-2 block h-12 w-80 rounded-lg bg-gray-700 p-2.5 text-white placeholder-gray-400 sm:text-sm"
+              // value=
+              // onChange={onHandleChange}
+              required
+            />
+            <div className="flex h-12 w-12 items-center justify-center rounded-full bg-indigo-950 text-center text-lg font-bold text-indigo-500">
+              <FontAwesomeIcon icon={faPaperPlane} />
+            </div>
+          </div>
         </div>
       </div>
     </div>
