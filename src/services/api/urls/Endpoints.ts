635263949ed5b578a3e81c59fe1735ccc4b236f6
Semeton <balogunsemeton@gmail.com>
2024-05-04 02:23:39 +0100

    core:Send and recieve messages base

--- src/services/api/urls/Endpoints.ts
+++ src/services/api/urls/Endpoints.ts
@@ -5,8 +5,10 @@ export const Endpoints = {
   verifyToken: "/users/verify/token",
   me: "/users/me",
   chats: "/chats",
+  chat: "/chats/show/",
   chatSecret: "/chats/secret/",
   receivedChatRequest: "/chats/requests",
   acceptRequest: "/chats/requests/accept/",
   chatMessages: "/chats/messages/",
+  sendMessage: "/chats/messages/",
 };
--- src/views/chat/components/ChatView.tsx
+++ src/views/chat/components/ChatView.tsx
@@ -3,35 +3,52 @@ import Http from "../../../services/handlers/Http";
 import { baseUrl } from "../../../services/api/urls/Links";
 import { Endpoints } from "../../../services/api/urls/Endpoints";
 import Alerts from "../../../util/alerts/Alerts";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {
   faArrowLeftLong,
   faGear,
   faPaperPlane,
+  faUserSecret,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import BarLoader from "../../components/BarLoader";
 
 const ChatView: React.FC = () => {
+  const [chatName, setChatName] = useState("");
+  const [me, setMe] = useState("");
+  const [chatMessages, setChatMessages] = useState([]);
+  const [loading, setLoading] = useState<Boolean>(true);
+  const [formdata, setFormdata] = useState<{ message: string }>({
+    message: "",
+  });
+
   const { id, secret } = useParams();
 
   const http = Http.getInstance();
+  const chatUrl = baseUrl + Endpoints.chat + id;
   const chatMessagesUrl = baseUrl + Endpoints.chatMessages;
+  const sendMessageUrl = baseUrl + Endpoints.sendMessage + id;
 
   const navigate = useNavigate();
 
   useEffect(
     () => {
       getChatMessages();
+      getChat();
+      setLoading(false);
     },
     // eslint-disable-next-line
     [],
   );
 
-  const getChatMessages = () => {
+  const getChat = () => {
+    setLoading(true);
     http
-      .get(chatMessagesUrl + id + "/" + secret)
+      .get(chatUrl)
       .then((res) => {
-        console.log(res);
+        res.role === "sender" ? setMe(res.sender_id) : setMe(res.recipient_id);
+        setChatName(res.other.name);
+        setLoading(false);
       })
       .catch((e) => {
         console.error(e);
@@ -40,9 +57,44 @@ const ChatView: React.FC = () => {
         navigate("/dashboard");
       });
   };
+  const getChatMessages = () => {
+    setLoading(true);
+    http
+      .get(chatMessagesUrl + id + "/" + secret)
+      .then((res) => {
+        setChatMessages(res);
+        setLoading(false);
+      })
+      .catch((e) => {
+        let message = e.response?.data?.message ?? e.message;
+        Alerts.error(message);
+        navigate("/dashboard");
+      });
+  };
+
+  const onHandleChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormdata({ ...formdata, [name]: value });
+  };
+
+  const sendMessage = () => {
+    const data: FormData = new FormData();
+    data.append("body", formdata.message);
+
+    http
+      .post(sendMessageUrl, data)
+      .then((res) => {
+        setFormdata({ ...formdata, message: "" });
+      })
+      .catch((e) => {
+        let message = e.response?.data?.message ?? e.message;
+        Alerts.error(message);
+      });
+  };
 
   return (
     <div className="fixed">
+      {loading && <BarLoader />}
       <div className="flex h-screen flex-col justify-between bg-gray-900">
         <header className="mb-2 grid grid-cols-3 items-center justify-between bg-gray-950 text-white">
           <div
@@ -53,7 +105,7 @@ const ChatView: React.FC = () => {
           </div>
           <div className="mx-auto">
             <div className="flex items-center justify-center rounded-full text-center text-lg font-bold text-indigo-500">
-              Full Name
+              {chatName}
             </div>
           </div>
           <div className="flex h-16 items-center justify-end p-6">
@@ -61,24 +113,47 @@ const ChatView: React.FC = () => {
           </div>
         </header>
         <main className="mb-auto mt-2 overflow-y-scroll">
-          <div className="float-left my-1 max-w-xs rounded-lg bg-indigo-500 p-2 text-white">
-            Message bubble on the left. Message bubble on the left
-          </div>
-          <div className="float-left my-1 max-w-xs rounded-lg bg-indigo-500 p-2 text-white">
-            Message bubble on the left. Message bubble on the left
-          </div>
-          <div className="float-right my-1 max-w-xs rounded-lg bg-gray-800 p-2 text-white">
-            Message bubble on the right. Message bubble on the right
-          </div>
-          <div className="float-left my-1 max-w-xs rounded-lg bg-indigo-500 p-2 text-white">
-            Message bubble on the left. Message bubble on the left
-          </div>
-          <div className="float-right my-1 max-w-xs rounded-lg bg-gray-800 p-2 text-white">
-            Message bubble on the right. Message bubble on the right
-          </div>
-          <div className="float-right my-1 max-w-xs rounded-lg bg-gray-800 p-2 text-white">
-            Message bubble on the right. Message bubble on the right
-          </div>
+          {loading ? (
+            <BarLoader />
+          ) : chatMessages.length === 0 ? (
+            <div className="mt-10 h-100 text-center text-white mx-5">
+              <div className="mb-3 mt-7 text-indigo-500">
+                <FontAwesomeIcon
+                  icon={faUserSecret}
+                  size="2xl"
+                  style={{ fontSize: "100px" }}
+                  className=""
+                />
+              </div>
+              <h3 className="text-2xl font-bold">Ooops!</h3>
+              <p className="">
+                You do not have any encrypted messages. <br />
+                Start typing with the box below
+              </p>
+            </div>
+          ) : (
+            <div className="block">
+              {chatMessages.map((message) => (
+                <div>
+                  {(message as any).user_id === me ? (
+                    <div className="float-left my-1 min-w-32 max-w-72 rounded-lg bg-indigo-500 p-2 text-white">
+                      {(message as any).message}
+                      <div className="text-right text-xs pl-5 opacity-70">
+                        {(message as any).time}
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="float-right my-1 min-w-32 max-w-72 rounded-lg bg-gray-800 p-2 text-white">
+                      {(message as any).message}
+                      <div className="text-right text-xs pl-7 opacity-70">
+                        {(message as any).time}
+                      </div>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          )}
         </main>
         <div className="bottom-0 h-24 w-screen bg-gray-950 pt-5">
           <div className="flex items-center justify-around text-center text-sm text-white">
@@ -87,13 +162,18 @@ const ChatView: React.FC = () => {
               name="message"
               id="message"
               className="focus:ring-primary-600 focus:border-primary-600 ml-2 block h-12 w-80 rounded-lg bg-gray-700 p-2.5 text-white placeholder-gray-400 sm:text-sm"
-              // value=
-              // onChange={onHandleChange}
+              value={formdata.message}
+              onChange={onHandleChange}
               required
             />
-            <div className="flex h-12 w-12 items-center justify-center rounded-full bg-indigo-950 text-center text-lg font-bold text-indigo-500">
-              <FontAwesomeIcon icon={faPaperPlane} />
-            </div>
+            {formdata.message.length > 0 && (
+              <div
+                className="flex h-12 w-12 items-center justify-center rounded-full bg-indigo-950 text-center text-lg font-bold text-indigo-500"
+                onClick={sendMessage}
+              >
+                <FontAwesomeIcon icon={faPaperPlane} />
+              </div>
+            )}
           </div>
         </div>
       </div>
