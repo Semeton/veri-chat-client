5dc76c255190a9b3045721cbc1147351437cf00f
Semeton <balogunsemeton@gmail.com>
2024-05-16 08:03:30 +0100

    mod: secret input modification

--- src/services/api/urls/Links.ts
+++ src/services/api/urls/Links.ts
@@ -1,3 +1,3 @@
-export const baseUrl = "https://verivault.test/api/v1";
-export const fileLink = "https://verivaul.test";
+export const baseUrl = "https://verivault.xyz/api/v1";
+export const fileLink = "https://verivaul.xyz";
 export const webSockectLink = "https://veri-chat-socket.onrender.com/";
--- src/util/alerts/Alerts.ts
+++ src/util/alerts/Alerts.ts
@@ -5,14 +5,14 @@ const Toast = Swal.mixin({
   toast: true,
   position: "top-end",
   showConfirmButton: false,
-  timer: 3000,
+  // timer: 3000,
   timerProgressBar: true,
   customClass: {
     popup: "colored-toast",
   },
   didOpen: (toast) => {
     toast.onmouseenter = Swal.stopTimer;
-    toast.onmouseleave = Swal.resumeTimer;
+    // toast.onmouseleave = Swal.resumeTimer;
   },
 });
 
